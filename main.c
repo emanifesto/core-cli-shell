d66d1b02bcf50c4668b58f40afb4b509a3a8affa
emanifesto <emmanuelbabayemi37@gmail.com>
2025-11-22 18:17:50 -0500

    implemented input/output redirection

--- main.c
+++ main.c
@@ -148,6 +148,32 @@ int execute(char **args) {
     pid = fork();
 
     if (pid == 0) {
+        // Output redirection: command > file
+        for (int i = 0; args[i] != NULL; i++) {
+            if (strcmp(args[i], ">") == 0) {
+                int fd = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (fd < 0) { perror("open"); exit(1); }
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+
+                args[i] = NULL;  // terminate args before '>'
+                break;
+            }
+        }
+
+        // Input redirection: command < file
+        for (int i = 0; args[i] != NULL; i++) {
+            if (strcmp(args[i], "<") == 0) {
+                int fd = open(args[i+1], O_RDONLY);
+                if (fd < 0) { perror("open"); exit(1); }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+
+                args[i] = NULL;  
+                break;
+            }
+        }
+
         // Child process: execute the command
         // NOTE: Need to strip newline from the last argument before execvp
         if (execvp(args[0], args) == -1) {
